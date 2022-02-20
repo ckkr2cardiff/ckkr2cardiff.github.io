36990ed28a3cc2ec5d9d17b75e5f39d516b6541b
Cardiff KRR <ckkr.cardiff@gmail.com>
2022-02-20 20:40:47 +0000

    update build

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@ make freeze
 echo "ckkr2cardiff.github.io" > build/CNAME
 git add build
 git add static
+git add sitedata
 git commit -am "update build"
 git push origin
 #git subtree push --prefix build origin gh-pages
